d59a7e261807302e15b206239e66c367f165a611
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-07 13:01:07 +0530

    feat: create Square.js component and link to Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,24 @@
 import React from "react";
 import "../App.css";
+import Square from "./Square.js";
 
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
---
+++ src/components/Square.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../App.css";
+
+export default function Square() {
+  return (
+    <>
+      <button className="square">1</button>
+    </>
+  );
+}
