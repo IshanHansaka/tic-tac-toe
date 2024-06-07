c1ea89cfd7b6db20a9f9aa7722c32cfde3d7959b
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-07 18:44:55 +0530

    chore: change code format

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../App.css";
 
-export default function Square({ value, onSquareClick}) {
+export default function Square({ value, onSquareClick }) {
   return (
     <>
-      <button className="square" onClick={onSquareClick}>{value}</button>
+      <button className="square" onClick={onSquareClick}>
+        {value}
+      </button>
     </>
   );
 }
